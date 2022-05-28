f983b83f49ddce196f56b30eb62b0de4111ed5c9
kim <yuyukgh@naver.com>
2022-05-29 03:13:22 +0900

    [Feat] nft 반응형 업데이트

--- src/pages/OPDLAB/index.js
+++ src/pages/OPDLAB/index.js
@@ -4,8 +4,28 @@ import { useState } from "react";
 import background_red from "../../assets/images/OPD_LAB/background_red.jpg";
 import background_blue from "../../assets/images/OPD_LAB/background_blue.jpg";
 import background_yello from "../../assets/images/OPD_LAB/background_yello.jpg";
+import boxRed from "../../assets/images/OPD_LAB/object_red.png";
+import boxBlue from "../../assets/images/OPD_LAB/object_blue.png";
+import boxYellow from "../../assets/images/OPD_LAB/object_yellow.png";
+import boxRedActive from "../../assets/images/OPD_LAB/object_red_active.png";
+import boxBlueActive from "../../assets/images/OPD_LAB/object_blue_active.png";
+import boxYellowActive from "../../assets/images/OPD_LAB/object_yellow_active.png";
 
-import { wrapper, background, hover_background, labNav, infoCSS, menuTitle, active } from "./style";
+import {
+  wrapper,
+  background,
+  hover_background,
+  labNav,
+  infoCSS,
+  menuTitle,
+  active,
+  wrapperMobile,
+  navMobile,
+  menu,
+  centerNav,
+  infoMobileCSS,
+  upY,
+} from "./style";
 
 const OPDLAB = () => {
   const data = [
@@ -27,7 +47,9 @@ const OPDLAB = () => {
   ];
   const [selectBg, setSelectBg] = useState(null);
   const [isActive, setIsActive] = useState(false);
+  const [isActiveM, setIsActiveM] = useState(true);
   const [activeIndex, setActiveIndex] = useState(0);
+  const [isClickObj, setIsClickObj] = useState({ red: false, blue: false, yellow: false });
 
   const onMouseLeave = () => {
     setSelectBg(null);
@@ -38,62 +60,167 @@ const OPDLAB = () => {
     setIsActive(true);
   };
 
+  // const handleMobileNavIsActive = () => {};
+
+  const handleIsClickObj = box => {
+    if (box === "red") {
+      setIsClickObj({ red: true, blue: false, yellow: false });
+    }
+    if (box === "blue") {
+      setIsClickObj({ red: false, blue: true, yellow: false });
+    }
+    if (box === "yellow") {
+      setIsClickObj({ red: false, blue: false, yellow: true });
+    }
+    if (box === "all") {
+      setIsClickObj({ red: false, blue: false, yellow: false });
+    }
+  };
   return (
-    <main css={wrapper}>
-      <ul css={labNav}>
-        <li
-          onMouseEnter={() => {
-            setSelectBg(background_red);
-          }}
-          onMouseLeave={onMouseLeave}
-          onClick={() => {
-            handleShowInfo(0);
-          }}
-          css={active(isActive)}
-        >
-          <span css={menuTitle("LAUNCH PAD")}>LAUNCH PAD</span>
-        </li>
-        <li
-          onMouseEnter={() => {
-            setSelectBg(background_blue);
-          }}
-          onMouseLeave={onMouseLeave}
-          onClick={() => {
-            handleShowInfo(1);
-          }}
-          css={active(isActive)}
-        >
-          <span css={menuTitle("NFT")}>NFT</span>
-        </li>
-        <li
-          onMouseEnter={() => {
-            setSelectBg(background_yello);
-          }}
-          onMouseLeave={onMouseLeave}
-          onClick={() => {
-            handleShowInfo(2);
-          }}
-          css={active(isActive)}
-        >
-          <span css={menuTitle("DAO")}>DAO</span>
-        </li>
-      </ul>
-      <div css={infoCSS(isActive)}>
-        <h3>{data[activeIndex].title}</h3>
-        <p>{data[activeIndex].desc}</p>
-        <a href={data[activeIndex].moreLink}>LEARN MORE &gt;</a>
-        <button
-          type="button"
-          onClick={() => {
-            setIsActive(false);
-          }}
-        >
-          X
-        </button>
-      </div>
-      <div css={hover_background} style={{ backgroundImage: `url(${selectBg})` }} />
-      <div css={background}></div>
-    </main>
+    <>
+      <main css={wrapper}>
+        <ul css={labNav}>
+          <li
+            onMouseEnter={() => {
+              setSelectBg(background_red);
+            }}
+            onMouseLeave={onMouseLeave}
+            onClick={() => {
+              handleShowInfo(0);
+            }}
+            css={active(isActive)}
+          >
+            <span css={menuTitle("LAUNCH PAD")}>LAUNCH PAD</span>
+          </li>
+          <li
+            onMouseEnter={() => {
+              setSelectBg(background_blue);
+            }}
+            onMouseLeave={onMouseLeave}
+            onClick={() => {
+              handleShowInfo(1);
+            }}
+            css={active(isActive)}
+          >
+            <span css={menuTitle("NFT")}>NFT</span>
+          </li>
+          <li
+            onMouseEnter={() => {
+              setSelectBg(background_yello);
+            }}
+            onMouseLeave={onMouseLeave}
+            onClick={() => {
+              handleShowInfo(2);
+            }}
+            css={active(isActive)}
+          >
+            <span css={menuTitle("DAO")}>DAO</span>
+          </li>
+        </ul>
+        <div css={infoCSS(isActive)}>
+          <h3>{data[activeIndex].title}</h3>
+          <p>{data[activeIndex].desc}</p>
+          <a href={data[activeIndex].moreLink}>LEARN MORE &gt;</a>
+          <button
+            type="button"
+            onClick={() => {
+              setIsActive(false);
+            }}
+          >
+            X
+          </button>
+        </div>
+        <div css={hover_background} style={{ backgroundImage: `url(${selectBg})` }} />
+        <div css={background}></div>
+      </main>
+      <main css={wrapperMobile}>
+        <ul css={centerNav}>
+          <li css={upY(isClickObj.red)}>
+            <img src={isClickObj.red ? boxRedActive : boxRed} alt="" />
+          </li>
+          <li css={upY(isClickObj.blue)}>
+            <img src={isClickObj.blue ? boxBlueActive : boxBlue} alt="" />
+          </li>
+          <li css={upY(isClickObj.yellow)}>
+            <img src={isClickObj.yellow ? boxYellowActive : boxYellow} alt="" />
+          </li>
+        </ul>
+        <ul css={navMobile(isActiveM)}>
+          <li
+            onClick={() => {
+              handleIsClickObj("red");
+              setIsActiveM(false);
+            }}
+          >
+            <span css={menu("LAUNCH PAD")}>LAUNCH PAD</span>
+          </li>
+          <li
+            onClick={() => {
+              handleIsClickObj("blue");
+              setIsActiveM(false);
+            }}
+          >
+            <span css={menu("NFT")}>NFT</span>
+          </li>
+          <li
+            onClick={() => {
+              handleIsClickObj("yellow");
+              setIsActiveM(false);
+            }}
+          >
+            <span css={menu("DAO")}>DAO</span>
+          </li>
+        </ul>
+        {isClickObj.red && (
+          <div css={infoMobileCSS}>
+            <h3>{data[0].title}</h3>
+            <p>{data[0].desc}</p>
+            <a href={data[0].moreLink}>LEARN MORE &gt;</a>
+            <button
+              type="button"
+              onClick={() => {
+                handleIsClickObj("all");
+                setIsActiveM(true);
+              }}
+            >
+              X
+            </button>
+          </div>
+        )}
+        {isClickObj.blue && (
+          <div css={infoMobileCSS}>
+            <h3>{data[1].title}</h3>
+            <p>{data[1].desc}</p>
+            <a href={data[1].moreLink}>LEARN MORE &gt;</a>
+            <button
+              type="button"
+              onClick={() => {
+                handleIsClickObj("all");
+                setIsActiveM(true);
+              }}
+            >
+              X
+            </button>
+          </div>
+        )}
+        {isClickObj.yellow && (
+          <div css={infoMobileCSS}>
+            <h3>{data[2].title}</h3>
+            <p>{data[2].desc}</p>
+            <a href={data[2].moreLink}>LEARN MORE &gt;</a>
+            <button
+              type="button"
+              onClick={() => {
+                handleIsClickObj("all");
+                setIsActiveM(true);
+              }}
+            >
+              X
+            </button>
+          </div>
+        )}
+      </main>
+    </>
   );
 };
 
--- src/pages/OPDLAB/style.js
+++ src/pages/OPDLAB/style.js
@@ -1,9 +1,11 @@
 import { css } from "@emotion/react";
-import { textEffect } from "../../assets/styles/common";
+import { MOBILE } from "../../assets/styles/mediaQuery";
+import { textEffect, backgroundImage } from "../../assets/styles/common";
 
 import OPD_background from "../../assets/images/OPD_LAB/background.jpg";
-// import OPD_background_MOBILE from "../../assets/images/OPD_LAB/background_mobile.jpg";
+import OPD_background_MOBILE from "../../assets/images/OPD_LAB/background_mobile.jpg";
 import text_frame from "../../assets/images/OPD_LAB/text_frame.png";
+import text_frame_mobile from "../../assets/images/OPD_LAB/text_frame_mobile.png";
 import text_background from "../../assets/images/OPD_LAB/text_background.png";
 import text_background_small from "../../assets/images/OPD_LAB/text_background_small.png";
 
@@ -11,6 +13,10 @@ export const wrapper = css`
   width: 100%;
   min-height: 100vh;
   position: relative;
+
+  ${MOBILE} {
+    display: none;
+  }
 `;
 
 export const background = css`
@@ -157,4 +163,117 @@ export const menuTitle = text => css`
   width: fit-content;
   opacity: 0;
   transition: opacity 0.5s ease;
+`;
+
+export const wrapperMobile = css`
+  width: 100%;
+  min-height: 100vh;
+  position: relative;
+  ${backgroundImage(OPD_background_MOBILE)};
+  background-position: center 100%;
+`;
+
+export const navMobile = boolean => css`
+  position: absolute;
+  bottom: 5%;
+  left: 50%;
+  transform: translate(-50%, 0);
+
+  ${!boolean &&
+  `
+  display:none;
+  `}
+
+  > li {
+    font-size: 7vw;
+    text-align: center;
+    white-space: nowrap;
+    background-image: url(${text_background});
+    background-position: center center;
+    background-size: contain;
+    background-repeat: no-repeat;
+    padding: 1rem 2rem;
+    cursor: pointer;
+  }
+`;
+
+export const menu = title => css`
+  color: #fff;
+  ${textEffect(title)};
+  transform: translate(-50%, 0);
+`;
+
+export const centerNav = css`
+  position: absolute;
+  top: 55%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 80%;
+  height: 30vw;
+  display: flex;
+  justify-content: space-between;
+
+  > li {
+    width: 30%;
+    height: 100%;
+    cursor: pointer;
+
+    > img {
+      width: 100%;
+    }
+  }
+`;
+
+export const infoMobileCSS = css`
+  position: absolute;
+  z-index: 10;
+  left: 50%;
+  bottom: 1rem;
+  padding: 2rem;
+  width: 80%;
+  height: auto;
+  transform: translate(-50%, 0);
+  background-image: url(${text_frame_mobile});
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  background-position: center center;
+
+  > h3 {
+    color: #fff;
+    font-size: 2rem;
+    padding-bottom: 0.5rem;
+    font-weight: 700;
+    text-shadow: 0px 0px 1.25em #8cfde959;
+  }
+
+  > p {
+    color: #fff;
+    font-size: 1.125rem;
+    line-height: 1.6;
+  }
+  > a {
+    margin-top: 2rem;
+    text-align: right;
+    display: block;
+    font-size: 1.25rem;
+    color: #fff;
+    text-shadow: 0px 0px 1.25em #00ecc2;
+  }
+  > button {
+    background-color: transparent;
+    position: absolute;
+    right: 2rem;
+    top: 2rem;
+    color: #fff;
+    font-size: 2rem;
+    cursor: pointer;
+    border: 0;
+    line-height: 1;
+    text-shadow: 0px 0px 1.25em #00ecc2;
+  }
+`;
+
+export const upY = boolean => css`
+  transition: all 0.2s ease;
+  transform: ${boolean ? "translateY(-2rem)" : "translateY(0)"};
 `;
