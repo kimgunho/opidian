7adc273f951a9853664b30316354b1268f4e7849
kim <yuyukgh@naver.com>
2022-05-28 23:29:36 +0900

    [Feat] map sucess
    - mobile
    - pc

--- src/pages/map/index.js
+++ src/pages/map/index.js
@@ -1,55 +1,68 @@
 /** @jsxImportSource @emotion/react */
+import rocket from "../../assets/images/MAP/rocket.png";
+import rocketVertical from "../../assets/images/MAP/rocket_vertical.png";
 
-import { wrapper, title, sections, mainModel, list } from "./style";
+import {
+  wrapper,
+  title,
+  sections,
+  list,
+  PCIMG,
+  MOBILEIMG,
+  mobileCenterTop,
+  mobileCenterBottom,
+} from "./style";
 const MAP = () => {
   return (
     <main css={wrapper}>
       <h2 css={title}>MAP</h2>
-      <div css={mainModel}>
-        <ul css={list}>
-          <li>
-            <h3>1st</h3>
-            <p>
-              Community Wallet
-              <br />
-              We enter the Sandbox
-              <br />
-              Opidian Social App
-              <br />
-              P2E Game + ICO
-            </p>
-          </li>
-          <li>
-            <h3>2nd</h3>
-            <p>
-              Community Wallet
-              <br />
-              We enter the Sandbox
-              <br />
-              Opidian Social App
-              <br />
-              P2E Game + ICO
-            </p>
-          </li>
-          <li>
-            <h3>3rd</h3>
-            <p>
-              Community Wallet
-              <br />
-              We enter the Sandbox
-              <br />
-              Opidian Social App
-              <br />
-              P2E Game + ICO
-            </p>
-          </li>
-        </ul>
-        <div></div>
-      </div>
 
       <div css={sections}>
         <div className="left"></div>
-        <div className="center"></div>
+        <div className="center">
+          <div css={mobileCenterTop} />
+          <ul css={list}>
+            <li>
+              <h3>1st</h3>
+              <p>
+                Community Wallet
+                <br />
+                We enter the Sandbox
+                <br />
+                Opidian Social App
+                <br />
+                P2E Game + ICO
+              </p>
+            </li>
+            <li>
+              <h3>2nd</h3>
+              <p>
+                Community Wallet
+                <br />
+                We enter the Sandbox
+                <br />
+                Opidian Social App
+                <br />
+                P2E Game + ICO
+              </p>
+            </li>
+            <li>
+              <h3>3rd</h3>
+              <p>
+                Community Wallet
+                <br />
+                We enter the Sandbox
+                <br />
+                Opidian Social App
+                <br />
+                P2E Game + ICO
+              </p>
+            </li>
+          </ul>
+          <img css={PCIMG} src={rocket} alt="" />
+          <img css={MOBILEIMG} src={rocketVertical} alt="" />
+          <div css={mobileCenterBottom} />
+        </div>
         <div className="right"></div>
       </div>
     </main>
--- src/pages/map/style.js
+++ src/pages/map/style.js
@@ -1,10 +1,13 @@
-import { css } from "@emotion/react";
+import { css, keyframes } from "@emotion/react";
 import { backgroundImage } from "../../assets/styles/common";
+import { MOBILE } from "../../assets/styles/mediaQuery";
+
 import background from "../../assets/images/MAP/background.jpg";
 import background_left from "../../assets/images/MAP/info_left.png";
 import background_right from "../../assets/images/MAP/info_right.png";
 import background_center from "../../assets/images/MAP/info_center.png";
-import rocket from "../../assets/images/MAP/rocket.png";
+import background_top from "../../assets/images/MAP/mobile_top.png";
+import background_bottom from "../../assets/images/MAP/mobile_bottom.png";
 
 export const wrapper = css`
   width: 100%;
@@ -21,6 +24,47 @@ export const title = css`
   color: #fff;
   font-size: 3.2rem;
   font-weight: 700;
+
+  ${MOBILE} {
+    font-size: 2.4rem;
+  }
+`;
+
+const fadeShow = keyframes`
+0%{
+  opacity:0;
+}
+8%{
+  opacity:.5;  
+}
+16%{
+  opacity:0;  
+}
+24%{
+  opacity:1;    
+  clip:rect(auto, 0, auto, auto) ;
+}
+100%{
+  clip:rect(auto, 1500px, auto, auto) ;  
+}
+`;
+
+const downMove = keyframes`
+0%{
+  height:0%;
+}
+100%{
+  height:100%;
+}
+`;
+
+const fadeOn = keyframes`
+0%{
+  opacity:0 ;
+}
+100%{
+  opacity:1 ;
+}
 `;
 
 export const sections = css`
@@ -31,65 +75,197 @@ export const sections = css`
   display: flex;
   justify-content: space-between;
 
+  ${MOBILE} {
+    align-items: center;
+  }
+
   > div {
-    width: 30%;
+    width: 20%;
     box-sizing: border-box;
+    filter: drop-shadow(2px 4px 6px black);
 
     &:nth-of-type(1) {
       ${backgroundImage(background_left)};
+      background-position: left center;
+      opacity: 0;
+      animation: ${fadeOn} 2s forwards ease-in-out;
+      animation-delay: 4s;
+
+      ${MOBILE} {
+        display: none;
+      }
     }
     &:nth-of-type(2) {
       ${backgroundImage(background_center)};
       background-size: contain;
       background-position: center bottom;
+      width: 55%;
+      position: relative;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      align-items: center;
+
+      ${MOBILE} {
+        width: 100%;
+        height: 90vh;
+        background-image: none;
+      }
+
+      > img {
+        width: 100%;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        animation: ${fadeShow} 3s forwards ease-in;
+
+        ${MOBILE} {
+          width: auto;
+          left: 20%;
+          top: 50%;
+          max-height: 65vh;
+        }
+      }
     }
     &:nth-of-type(3) {
       ${backgroundImage(background_right)};
+      background-position: right center;
+      opacity: 0;
+      animation: ${fadeOn} 2s forwards ease-in-out;
+      animation-delay: 4s;
+
+      ${MOBILE} {
+        display: none;
+      }
     }
   }
 `;
 
-export const mainModel = css`
-  position: absolute;
-  left: 50%;
-  top: 50%;
+export const mobileCenterTop = css`
+  display: none;
   width: 100%;
-  max-width: 60vw;
-  height: 100%;
-  max-height: 60vh;
-  transform: translate(-50%, -50%);
-  padding: 1rem;
+  height: 8vh;
+  ${backgroundImage(background_top)};
+  background-position: center bottom;
+  background-size: contain;
 
-  > div {
-    width: 100%;
-    height: 50%;
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-    ${backgroundImage(rocket)};
-    background-size: contain;
+  ${MOBILE} {
+    display: block;
+  }
+`;
+export const mobileCenterBottom = css`
+  display: none;
+  width: 100%;
+  height: 10vh;
+  ${backgroundImage(background_bottom)};
+  background-position: center top;
+  background-size: auto 10vh;
+
+  ${MOBILE} {
+    display: block;
   }
 `;
 
 export const list = css`
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 0%;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  animation: ${downMove} 1s forwards ease-in-out;
+  animation-delay: 3s;
+
   > li {
-    padding-left: 1rem;
-    border-left: 2px solid #fff;
+    padding-left: 3rem;
+    border-left: 3px solid #fff;
     position: absolute;
 
+    ${MOBILE} {
+      border-left: 0;
+      border-top: 3px solid #fff;
+      padding: 1rem 0 0 0;
+    }
+
+    &::after {
+      content: "";
+      position: absolute;
+      left: -2px;
+      width: 1px;
+      background-color: #fff;
+      opacity: 0.4;
+
+      ${MOBILE} {
+        top: -2px;
+        width: 20vw;
+        left: auto;
+        right: 100%;
+      }
+    }
+
     &:nth-of-type(1) {
-      left: 2rem;
-      top: 2rem;
+      left: 0;
+      top: 20%;
+
+      ${MOBILE} {
+        top: 15%;
+        right: 0;
+        left: auto;
+      }
+
+      &::after {
+        height: 10vh;
+        bottom: -10vh;
+
+        ${MOBILE} {
+          bottom: auto;
+          height: 1px;
+        }
+      }
     }
     &:nth-of-type(2) {
-      left: 50%;
-      bottom: 0;
-      transform: translate(-50%, 0);
+      bottom: 20%;
+      left: 33%;
+
+      ${MOBILE} {
+        top: 40%;
+        right: 0;
+        left: auto;
+      }
+
+      &::after {
+        height: 5vh;
+        top: -5vh;
+
+        ${MOBILE} {
+          top: -2px;
+          height: 1px;
+          bottom: auto;
+        }
+      }
     }
     &:nth-of-type(3) {
-      right: 2rem;
-      top: 2rem;
+      left: 66%;
+      top: 20%;
+
+      ${MOBILE} {
+        top: 65%;
+        right: 0;
+        left: auto;
+      }
+
+      &::after {
+        height: 5vh;
+        bottom: -5vh;
+
+        ${MOBILE} {
+          height: 1px;
+          bottom: auto;
+        }
+      }
     }
 
     > h3 {
@@ -102,5 +278,17 @@ export const list = css`
       font-size: 1rem;
       line-height: 2;
     }
+  }
+`;
+
+export const PCIMG = css`
+  ${MOBILE} {
+    display: none;
+  }
+`;
+export const MOBILEIMG = css`
+  display: none;
+  ${MOBILE} {
+    display: block;
   }
 `;
